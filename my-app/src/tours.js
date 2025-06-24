50002d94a00df66a3400050f80a7bd8b87d7afe5
Bernard Krehula <krehula.bernard456@gmail.com>
2025-06-24 21:19:31 +0200

    creating tours array

---
+++ my-app/src/tours.js
@@ -0,0 +1,44 @@
+const tours = [
+    {
+        id: '',
+        price: '1,995',
+        title: 'Best of Paris in 7 Days Tour',
+        info: "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+        isInterested: '',
+        image: ''
+    },
+    {
+        id: '',
+        price: '3,895',
+        title: 'Best of Ireland in 14 Days Tour',
+        info: '',
+        isInterested: '',
+        image: ''
+    },
+    {
+        id: '',
+        price: '2,695',
+        title: '',
+        info: '',
+        isInterested: '',
+        image: ''
+    },
+    {
+        id: '',
+        price: '2,095',
+        title: '',
+        info: '',
+        isInterested: '',
+        image: ''
+    },
+    {
+        id: '',
+        price: '2,595',
+        title: '',
+        info: '',
+        isInterested: '',
+        image: ''
+    }
+]
+
+export default tours;
