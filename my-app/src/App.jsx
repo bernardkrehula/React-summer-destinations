819de489684823dbfa3c4c083f20167bb6f5e6a0
Bernard Krehula <krehula.bernard456@gmail.com>
2025-06-25 18:51:35 +0200

    Set design

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='main'>
+        <h1>Our Tours</h1>
+        <ul className='tours'>
+          <li className='tour'>
+            <img src='https://www.course-api.com/images/tours/tour-1.jpeg'></img>
+            <h4>$1000</h4>
+            <h2>Best of Paris in 7 Days Tour</h2>
+            <p>Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!</p>
+            <h3>Read More</h3>
+            <button>Not interested</button>
+          </li>
+          <li className='tour'>
+            <img src='https://www.course-api.com/images/tours/tour-1.jpeg'></img>
+            <h4>$1000</h4>
+            <h2>Best of Paris in 7 Days Tour</h2>
+            <p>Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!</p>
+            <h3>Read More</h3>
+            <button>Not interested</button>
+          </li>
+          <li className='tour'>
+            <img src='https://www.course-api.com/images/tours/tour-1.jpeg'></img>
+            <h4>$1000</h4>
+            <h2>Best of Paris in 7 Days Tour</h2>
+            <p>Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!</p>
+            <h3>Read More</h3>
+            <button>Not interested</button>
+          </li>
+          <li className='tour'>
+            <img src='https://www.course-api.com/images/tours/tour-1.jpeg'></img>
+            <h4>$1000</h4>
+            <h2>Best of Paris in 7 Days Tour</h2>
+            <p>Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!</p>
+            <h3>Read More</h3>
+            <button>Not interested</button>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
